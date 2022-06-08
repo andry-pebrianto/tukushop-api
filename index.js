6befdedcaa7bb3e8837f236c0762adc2b4d72e9f
edoprayoga1999 <edoprayoga.se@gmail.com>
2022-06-08 13:59:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const socketIo = require("socket.io");
+const http = require("http");
+const socketController = require("./src/socket");
+
+// route here
+
+const app = express();
+app.use(cors());
+app.use(helmet({
+	crossOriginResourcePolicy: false,
+}));
+app.use(xss());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+	res.json({message: "Hello World"});
+});
+app.use(express.static("public"));
+
+const APP_PORT = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+	cors: {
+		origin: "*"
+	}
+});
+io.on("connection", (socket) => {
+	console.log("new user connected");
+	socketController(io, socket);
+});
+server.listen(APP_PORT, () => {
+	console.log(`Service running on port ${APP_PORT}`);
+});
