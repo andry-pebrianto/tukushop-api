334e8edf3ab091ecc0624a99e7d132a99385bcd4
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-09 15:01:00 +0700

    Menambahkan validasi untuk update seller & buyer

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const validation = require("../validations/user.validation");
-// const runValidation = require("../middleware/runValidation");
+const validation = require("../validations/user.validation");
+const runValidation = require("../middleware/runValidation");
 const { editProfileBuyer, editProfileSeller, getListBuyer, getListSeller } = require("../controllers/user.controller");
 const jwtAuth = require("../middleware/jwtAuth");
 const upload = require("../middleware/upload");
@@ -11,7 +11,7 @@ const router = express.Router();
 router
 	.get("/user/buyer", jwtAuth, onlyAdmin, getListBuyer)
 	.get("/user/seller", jwtAuth, onlyAdmin, getListSeller)
-	.put("/user/:id/buyer", jwtAuth, myself, upload, editProfileBuyer)
-	.put("/user/:id/seller", jwtAuth, myself, upload, editProfileSeller);
+	.put("/user/:id/buyer", jwtAuth, myself, upload, validation.buyer, runValidation, editProfileBuyer)
+	.put("/user/:id/seller", jwtAuth, myself, upload, validation.seller, runValidation, editProfileSeller);
 
 module.exports = router;
