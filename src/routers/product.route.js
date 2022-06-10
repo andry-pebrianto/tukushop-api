30d3f1109cbd5f81111b06d5d0466b25d7fcaa92
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-10 17:12:17 +0700

    Menambahkan validasi untuk insert product

--- src/routers/product.route.js
+++ src/routers/product.route.js
@@ -1,14 +1,21 @@
 const express = require("express");
-// const validation = require("../validations/user.validation");
-// const runValidation = require("../middleware/runValidation");
+const validation = require("../validations/product.validation");
+const runValidation = require("../middleware/runValidation");
 const { addProduct } = require("../controllers/product.controller");
 const jwtAuth = require("../middleware/jwtAuth");
 const multipleUpload = require("../middleware/multipleUpload");
-const {onlySeller} = require("../middleware/authorization");
+const { onlySeller } = require("../middleware/authorization");
 
 const router = express.Router();
 
-router
-	.post("/product", jwtAuth, onlySeller, multipleUpload, addProduct);
+router.post(
+	"/product",
+	jwtAuth,
+	onlySeller,
+	multipleUpload,
+	validation.insert,
+	runValidation,
+	addProduct
+);
 
 module.exports = router;
---
+++ src/validations/product.validation.js
@@ -0,0 +1,20 @@
+const { check } = require("express-validator");
+
+const insert = [
+	// productName
+	check("productName", "Product Name required").not().isEmpty(),
+	check("productName", "Product Name maximum length is 100 characters").isLength({ max: 100 }),
+	// price
+	check("price", "Price required").not().isEmpty(),
+	check("price", "Price only can contains number").isNumeric(),
+	check("price", "Price too high").isLength({ max: 13 }),
+	// description
+	check("description", "Description maximum length is 250 characters").isLength({ max: 250 }),
+	// stock
+	check("stock", "Stock only can contains number").isNumeric(),
+	check("stock", "Stock too much").isLength({ max: 5 }),
+];
+
+module.exports = {
+	insert,
+};
