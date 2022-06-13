990dfc3ae811476e63a571189a7e70a643a70acc
Denny Wahyu <janexmgd@gmail.com>
2022-06-13 14:59:25 +0700

    fix response get my cart

--- src/routers/address.route.js
+++ src/routers/address.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 // import controller
 const {
-  addAddress,
-  myAddress,
-  updateMyAddress,
-  deleteAddress,
+	addAddress,
+	myAddress,
+	updateMyAddress,
+	deleteAddress,
 } = require("../controllers/address.controller");
 
 // import middleware
@@ -15,29 +15,29 @@ const runValidation = require("../middleware/runValidation");
 
 // import validation rules
 const {
-  createValidation,
-  updateValidation,
+	createValidation,
+	updateValidation,
 } = require("../validations/address.validation");
 
 const router = express.Router();
 
 router
-  .get("/my-address", jwtAuth, onlyBuyer, myAddress) // get my address buyer only
-  .post(
-    "/address",
-    jwtAuth,
-    onlyBuyer,
-    createValidation,
-    runValidation,
-    addAddress
-  ) // add new address buyer only
-  .put(
-    "/address/:id",
-    jwtAuth,
-    onlyBuyer,
-    updateValidation,
-    runValidation,
-    updateMyAddress
-  ) // update address owner address only
-  .delete("/address/:id", jwtAuth, onlyBuyer, deleteAddress); // delete address owner only
+	.get("/my-address", jwtAuth, onlyBuyer, myAddress) // get my address buyer only
+	.post(
+		"/address",
+		jwtAuth,
+		onlyBuyer,
+		createValidation,
+		runValidation,
+		addAddress
+	) // add new address buyer only
+	.put(
+		"/address/:id",
+		jwtAuth,
+		onlyBuyer,
+		updateValidation,
+		runValidation,
+		updateMyAddress
+	) // update address owner address only
+	.delete("/address/:id", jwtAuth, onlyBuyer, deleteAddress); // delete address owner only
 module.exports = router;
