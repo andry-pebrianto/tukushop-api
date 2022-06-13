ed37aa8897423869ec417ec1cb50d771bf5840f5
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-13 11:39:03 +0700

    Membuat endpoint untuk get list seller chat dan get list buyer chat

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const validation = require("../validations/user.validation");
 const runValidation = require("../middleware/runValidation");
-const { editProfileBuyer, editProfileSeller, getListBuyer, getListSeller, getDetailUser } = require("../controllers/user.controller");
+const { editProfileBuyer, editProfileSeller, getListBuyer, getListSeller, getDetailUser, getListChatSeller, getListChatBuyer } = require("../controllers/user.controller");
 const jwtAuth = require("../middleware/jwtAuth");
 const upload = require("../middleware/upload");
-const {onlyAdmin, myself} = require("../middleware/authorization");
+const {onlyAdmin, myself, onlyBuyer, onlySeller} = require("../middleware/authorization");
 
 const router = express.Router();
 
 router
 	.get("/user/buyer", jwtAuth, onlyAdmin, getListBuyer)
 	.get("/user/seller", jwtAuth, onlyAdmin, getListSeller)
+	.get("/user/chat/seller", jwtAuth, onlySeller, getListChatSeller)
+	.get("/user/chat/buyer", jwtAuth, onlyBuyer, getListChatBuyer)
 	.get("/user/:id", jwtAuth, getDetailUser)
 	.put("/user/:id/buyer", jwtAuth, myself, upload, validation.buyer, runValidation, editProfileBuyer)
 	.put("/user/:id/seller", jwtAuth, myself, upload, validation.seller, runValidation, editProfileSeller);
